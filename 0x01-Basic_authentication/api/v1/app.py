fa4f5d052dc887b2469f4847b14d8ddc2c4cece4
sotoriz <sotorisfx@gmail.com>
2023-08-07 14:15:54 -0700

    Request validation!

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -13,6 +13,11 @@ app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
+auth = None
+
+from api.v1.auth.auth import Auth
+auth = Auth()
+
 
 @app.errorhandler(404)
 def not_found(error) -> str:
@@ -32,6 +37,22 @@ def forbidden_error(error) -> str:
     """
     return jsonify({"error": "Forbidden"}), 403
 
+@app.before_request
+def request_filter() -> None:
+    """ Checks if request needs authorization
+    """
+    excluded_paths = [
+        '/api/v1/status/',
+        '/api/v1/unauthorized/',
+        '/api/v1/forbidden/'
+        ]
+
+    if auth:
+        if auth.require_auth(request.path, excluded_paths):
+            if auth.authorization_header(request) is None:
+                abort(401)
+            if auth.current_user(request) is None:
+                abort(403)
 
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
