0c33fbbe153155143eb156ead78cd7e06552d2b7
sotoriz <sotorisfx@gmail.com>
2023-08-10 01:12:08 -0700

    session authentication

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -55,15 +55,15 @@ def request_filter() -> None:
         '/api/v1/status/',
         '/api/v1/unauthorized/',
         '/api/v1/forbidden/'
+        '/api/v1/auth_session/login/'
         ]
 
-    if auth:
-        if auth.require_auth(request.path, excluded_paths):
-            if auth.authorization_header(request) is None:
-                abort(401)
-            if auth.current_user(request) is None:
-                abort(403)
-            request.current_user = auth.current_user(request)
+    if auth and auth.require_auth(request.path, excluded_paths):
+        if auth.authorization_header(request) is None and auth.session_cookie(request) is None:
+            abort(401)
+        if auth.current_user(request) is None:
+            abort(403)
+        request.current_user = auth.current_user(request)
 
 
 if __name__ == "__main__":
