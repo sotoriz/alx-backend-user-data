5cb20a3f65932a73137b452f454fe79bc2d0989a
sotoriz <sotorisfx@gmail.com>
2023-08-10 02:32:32 -0700

    session authentication

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -59,9 +59,11 @@ def request_filter() -> None:
         ]
 
     if auth and auth.require_auth(request.path, excluded_paths):
+
         if auth.authorization_header(request) is None \
                 and auth.session_cookie(request) is None:
             abort(401)
+
         if auth.current_user(request) is None:
             abort(403)
         request.current_user = auth.current_user(request)
