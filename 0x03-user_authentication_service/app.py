b55a133b728bf1485f17cfb3b98fbd52b19c312b
sotoriz <sotorisfx@gmail.com>
2023-08-16 10:56:27 -0700

    flask app

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,7 +1,8 @@
-#!/#!/usr/bin/env python3:wq
+#!/usr/bin/env python3
 """ Route module for the API """
 
 from flask import Flask, jsonify, request,  abort, redirect, url_for
+from sqlalchemy.orm.exc import NoResultFound
 from auth import Auth
 
 
