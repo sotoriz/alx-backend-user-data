00e6338786f607b48aaea0f314c5733e93e27ba0
sotoriz <sotorisfx@gmail.com>
2023-08-16 10:27:12 -0700

    create user, find user and update user

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -6,7 +6,7 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
 from sqlalchemy.exc import InvalidRequestError
 from sqlalchemy.orm.exc import NoResultFound
-
+from typing import TypeVar
 from user import Base, User
 
 
