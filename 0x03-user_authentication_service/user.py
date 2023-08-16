afdd3f2034527aa44e2b9bd407a2caeaaa1ac129
sotoriz <sotorisfx@gmail.com>
2023-08-16 10:09:47 -0700

    user model

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -1,11 +1,8 @@
 #!/usr/bin/env python3
-"""
-SQLAlchemy model for User
-"""
+"""SQLAlchemy model for User"""
 
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String
-
 Base = declarative_base()
 
 
