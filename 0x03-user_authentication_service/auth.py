2e26848f40a94b27716c3c1f88090e6c81c6818f
sotoriz <sotorisfx@gmail.com>
2023-08-16 11:09:41 -0700

    password and  validation

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -16,9 +16,6 @@ def _hash_password(password: str) -> str:
     return hashpw(password.encode('utf-8'), gensalt())
 
 
-from db import DB
-
-
 def _generate_uuid() -> str:
     """ Generates UUID
     Returns string representation of new UUID
@@ -76,14 +73,14 @@ class Auth:
             return None
 
     def destroy_session(self, user_id: str) -> None:
-            """ Updates user's session_id to None"""
-            if user_id is None:
-                return None
-            try:
-                found_user = self._db.find_user_by(id=user_id)
-                self._db.update_user(found_user.id, session_id=None)
-            except NoResultFound:
-                return None
+        """ Updates user's session_id to None"""
+        if user_id is None:
+            return None
+        try:
+            found_user = self._db.find_user_by(id=user_id)
+            self._db.update_user(found_user.id, session_id=None)
+        except NoResultFound:
+            return None
 
     def get_reset_password_token(self, email: str) -> str:
         """ Finds user by email, updates user's reset_toke with UUID """
